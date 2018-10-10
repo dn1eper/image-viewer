da5af799c635f0cbd93836f290926b8403d2e93a
Andrey <dn6pr0@gmail.com>
2018-10-10 17:59:16 +0300

    1. Реализован сервис просмотра изображений

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Image Viewer',
+    photo: fs.readdirSync('./public/photo/')
+  });
 });
 
 module.exports = router;
